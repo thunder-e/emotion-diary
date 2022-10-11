43525be27acc7593855ff08b6329e95215beee08
thunder-e <79903353+thunder-e@users.noreply.github.com>
2022-10-11 11:07:13 +0900

    ✨ /New

--- src/App.js
+++ src/App.js
@@ -73,8 +73,6 @@ const dummyData = [
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyData);
 
-  console.log(new Date().getTime());
-
   const dataId = useRef(0);
   // CREATE
   const onCreate = (date, content, emotion) => {
